57962d5933bddcdfe7e18399c6a58baf305b4eca
Chetan Shenoy <me@chetanshenoy.com>
2013-06-04 19:46:17 -0400

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Chetan Shenoy on 6/4/13.
+//  Copyright (c) 2013 Chetan Shenoy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
